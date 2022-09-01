28b0c3b0ff11af93c27eaedbc819a409685cb60f
ojcaje <ojcaje@gmail.com>
2022-08-31 21:09:01 -0400

    Módulo 8 - Mejoras

--- src/app/service/ficha-clinica-service.service.ts
+++ src/app/service/ficha-clinica-service.service.ts
@@ -5,6 +5,21 @@ import {HttpService} from "./http.service";
 import {FichaClinica} from "../model/ficha-clinica";
 import {Observable} from "rxjs";
 
+// interfaz para poder postear una ficha clínica
+interface FichaClinicaPost {
+  motivoConsulta: string;
+  diagnostico: string;
+  observacion: string;
+  idEmpleado: {idPersona: number};
+  idCliente: {idPersona: number};
+  idTipoProducto: {idTipoProducto: number};
+}
+
+// interfaz para hacer put de una ficha clinica
+interface FichaClinicaPut {
+  idFichaClinica: number;
+  observacion: string
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -15,18 +30,34 @@ export class FichaClinicaServiceService extends HttpService<FichaClinica,number>
   }
 
   // override hecho para poder ponerle el usuario en el header
-  override post(t:FichaClinica): Observable<FichaClinica>{
+  override post(t:FichaClinica | FichaClinicaPost): Observable<FichaClinica>{
     const headers: HttpHeaders = new HttpHeaders()
       .append('usuario','usuario2')
 
-    return this.httpClient.post<FichaClinica>(this.url+ "/"   + this.endpoint, t,{headers:headers});
+    // para que el back pueda aceptar la FichaClinica
+    const sentData: FichaClinicaPost = {
+      motivoConsulta: t.motivoConsulta,
+      diagnostico: t.diagnostico,
+      observacion: t.observacion,
+      idEmpleado: {idPersona: t.idEmpleado.idPersona},
+      idCliente: {idPersona: t.idCliente.idPersona},
+      idTipoProducto: {idTipoProducto: t.idTipoProducto.idTipoProducto}
+    }
+
+    return this.httpClient.post<FichaClinica>(this.url+ "/"   + this.endpoint, sentData,{headers:headers});
   }
 
   // override hecho para poder poner el usuario en el header
-  override put(t: FichaClinica): Observable<FichaClinica> {
+  override put(t: FichaClinica | FichaClinicaPut): Observable<FichaClinica> {
     const headers: HttpHeaders = new HttpHeaders()
       .append('usuario','usuario2')
 
-    return this.httpClient.put<FichaClinica>(this.url+ "/"   + this.endpoint, t,{headers:headers});
+    // para que el back pueda aceptar la FichaClinica
+    const sentData: FichaClinicaPut = {
+      idFichaClinica: t.idFichaClinica,
+      observacion: t.observacion
+    }
+
+    return this.httpClient.put<FichaClinica>(this.url+ "/"   + this.endpoint, sentData,{headers:headers});
   }
 }
